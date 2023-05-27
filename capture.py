99493b0aa0bf3e07af565e699d421e1f86690541
hamidreza <htalatian@gmail.com>
2023-05-27 16:55:36 +0430

    opencv

--- capture.py
+++ capture.py
@@ -3,7 +3,19 @@ from deffcode import FFdecoder
 
 
 # initialize and formulate the decoder with "0" index source for BGR24 output
-decoder = FFdecoder("0", frame_format="bgr24", verbose=True).formulate()
+
+ffparams = {
+    "-vcodec": "h264_cuvid",  # use H.264 CUVID Video-decoder
+    "-enforce_cv_patch": True # enable OpenCV patch for YUV(YUV420p) frames
+}
+
+# initialize and formulate the decoder with `foo.mp4` source
+decoder = FFdecoder(
+    "0",
+    frame_format="yuv420p",  # use YUV420p frame pixel format
+    verbose=True, # enable verbose output
+    **ffparams # apply various params and custom filters
+).formulate()
 
 # grab the BGR24 frames from decoder
 for frame in decoder.generateFrame():
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 xhost + &&
-sudo docker run -it --device=/dev/video0:/dev/video0 -v .:/code  -e HTTP_PROXY=$HTTP_PROXY -e HTTPS_PROXY=$HTTPS_PROXY gpu bash 
+sudo docker run -it --device=/dev/video0:/dev/video0 -v .:/code  -e HTTP_PROXY=$HTTP_PROXY -e HTTPS_PROXY=$HTTPS_PROXY htalatian/gpu:latest bash 
